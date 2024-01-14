47c08583414f1d6d625d9e5d2c1dfe4c209f3cfb
JesusG <py3user22@github.com>
2024-01-14 01:14:36 -0600

    0114 added serializer.py

--- app0114/views.py
+++ app0114/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import MenuItemSerializer
+from .models import MenuItem
 
-# Create your views here.
+
 def home(request):
     return render(request, '0114_django_demo.html', {})
+
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
 
