aa712750cecb08a1625ee539a9c079f0ba7a1c7f
JesusG <py3user22@github.com>
2024-01-15 01:19:38 -0600

    0115 added views.py from and new method

--- app0114/urls.py
+++ app0114/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('', views.home),
     path('menu-items', views.MenuItemView.as_view()),
     path('menu-items/', views.MenuItemView.as_view()),
+    path('api/menu-items/', views.MenuItemView.as_view()),
     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('api/menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('secret', views.secret),
+    path('api/secret/', views.secret),
 ]
--- app0114/views.py
+++ app0114/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
-from rest_framework import generics
+from django.shortcuts import render, get_object_or_404
+from rest_framework import generics, status
 from .serializers import MenuItemSerializer
 from .models import MenuItem
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 
 def home(request):
     return render(request, '0114_django_demo.html', {})
-
 
 class MenuItemView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
@@ -16,4 +18,7 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
 
-
+# 0115 adding method
+@api_view()
+def secret(request):
+    return Response({"message":"secret message @011524' @0110"})
