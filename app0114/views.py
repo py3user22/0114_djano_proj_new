763d583390debdd14423186b86e4b209d233f7d5
JesusG <py3user22@github.com>
2024-01-15 02:03:37 -0600

    0115 good save and server output

--- app0114/views.py
+++ app0114/views.py
@@ -5,6 +5,7 @@ from .models import MenuItem
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from django.core.paginator import Paginator, EmptyPage
 
 
 def home(request):
